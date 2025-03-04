571cbfc2dbd862d28dcec45fc48ccf2ab4a68861
Eduardo_H. <157440749+Ed-HV@users.noreply.github.com>
2025-03-04 15:38:57 -0600

    New style  (#33)
    Co-authored-by: Termius-hydra <set_gnu@outlook.com>
    Co-authored-by: Emonuel935 <oscarelite125@gmail.com>

--- user/dashboard.php
+++ user/dashboard.php
@@ -16,7 +16,7 @@ $nombre_usuario = $_SESSION['nombre'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard del Usuario - Biblioteca</title>
-
+    <link rel="stylesheet" href="assets/css/dashboardStyle.css"> 
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap Icons -->
